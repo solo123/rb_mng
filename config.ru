675e1a719836cfaf41c7638fb37bed0fadea9e6d
jimmy liang <576735357@qq.com>
2022-04-04 12:24:30 +0800

    helper

--- config.ru
+++ config.ru
@@ -3,7 +3,7 @@ require 'mongoid'
 
 require_all %w[init app lib helper model]
 
-puts ">> server start..."
+#puts ">> server start..."
 #use Ns::LogJsonReq
 #run Ns::App.freeze.app
 #run Ns::AppHtml.freeze.app
--- config/deploy.rb
+++ config/deploy.rb
@@ -12,21 +12,12 @@ set :branch, 'master'
 set :user, 'jimmy'          # Username in the server to SSH to.
 set :forward_agent, true     # SSH forward_agent.
 
-# Shared dirs and files will be symlinked into the app-folder by the 'deploy:link_shared_paths' step.
-# Some plugins already add folders to shared_dirs like `mina/rails` add `public/assets`, `vendor/bundle` and many more
 # run `mina -d` to see all folders and files already included in `shared_dirs` and `shared_files`
 set :shared_dirs, fetch(:shared_dirs, []).push('vendor/bundle')
 set :shared_files, fetch(:shared_files, []).push('.ruby-version', 'config/mongoid.yml')
 
-# This task is the environment that is loaded for all remote run commands, such as
-# `mina deploy` or `mina rake`.
 task :remote_environment do
-  # If you're using rbenv, use this to load the rbenv environment.
-  # Be sure to commit your .ruby-version or .rbenv-version to your repository.
   invoke :'rbenv:load'
-
-  # For those using RVM, use this to load an RVM version@gemset.
-  # invoke :'rvm:use', 'ruby-2.5.3@default'
 end
 
 # Put any custom commands you need to run at setup
@@ -39,55 +30,40 @@ end
 
 desc "Deploys the current version to the server."
 task :deploy do
-  # uncomment this line to make sure you pushed your local branch to the remote origin
-  #invoke :'git:ensure_pushed'
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
-    #invoke :'bundle:install'
-    #invoke :'rails:db_migrate'
-    #invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     on :launch do
       in_path(fetch(:current_path)) do
-        command %{cp ../shared/falcon.rb .}
-        #command %{mkdir -p tmp/}
-        #command %{touch tmp/restart.txt}
+        command %{cp ~/work/pooul_mng/shared/falcon.rb .}
+        command %{mkdir -p tmp/}
+        #command %{bundle}
+        command %{eye restart app:mng:test}
       end
     end
   end
-
-  # you can use `run :local` to run tasks on local machine before of after the deploy scripts
   run(:local){ command  "echo 'deploy done.'" }
 end
 
-desc "Deploys the current version to the server."
+desc "Deploys the current TEST version to the server."
 task :deploy_test do
   set :deploy_to, '/home/jimmy/test/pooul_mng'
-  # uncomment this line to make sure you pushed your local branch to the remote origin
-  #invoke :'git:ensure_pushed'
   deploy do
-    # Put things that will set up an empty directory into a fully set-up
-    # instance of your project.
     invoke :'git:clone'
     invoke :'deploy:link_shared_paths'
-    #invoke :'bundle:install'
-    #invoke :'rails:db_migrate'
-    #invoke :'rails:assets_precompile'
     invoke :'deploy:cleanup'
 
     on :launch do
       in_path(fetch(:current_path)) do
-        #command %{mkdir -p tmp/}
-        #command %{touch tmp/restart.txt}
+        command %{cp ~/test/pooul_mng/shared/falcon.rb .}
+        command %{mkdir -p tmp/}
+        #command %{bundle}
+        command %{eye restart app:mng:test}
       end
     end
   end
-
-  # you can use `run :local` to run tasks on local machine before of after the deploy scripts
   run(:local){ command  "echo 'deploy done.'" }
 end
 # For help in making your deploy script, see the Mina documentation:
