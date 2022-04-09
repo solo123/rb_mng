20f1d05483f99442b319ec82b460a2f1a957d302
jimmy liang <576735357@qq.com>
2022-04-10 00:52:54 +0800

    add refunds query

--- app/settle.rb
+++ app/settle.rb
@@ -9,17 +9,20 @@ module Ns
           Ns::Settlement.find_by(s_date: dt.to_date).to_json
         }
         r.on("errors") {
-          r.get("orders", String) { |dt|
-            Ns::PayOrder.where(s_date: dt.to_date).and(:settle.ne => 1).limit(100).all.to_json
-          }
           r.get("statements", String) { |dt|
             Ns::ChannelStatement.where(w_date: dt.to_date).and(:settle.ne => 1).limit(100).all.to_json
           }
           r.get("statements", String, String) { |dt, route|
             Ns::ChannelStatement.where(w_date: dt.to_date).and(:settle.ne => 1).and(route: route.classify).limit(100).all.to_json
           }
+          r.get("orders", String) { |dt|
+            Ns::PayOrder.where(s_date: dt.to_date).and(:settle.ne => 1).limit(100).all.to_json
+          }
           r.get("orders", String, String) { |dt, route|
             Ns::PayOrder.where(s_date: dt.to_date).and(:settle.ne => 1).and(doc_type: "Ns::PayOrder::#{route.classify}").limit(100).all.to_json
+          }
+          r.get("refunds", String) { |dt|
+            Ns::Refund.where(s_date: dt.to_date).and(:settle.ne => 1).limit(100).all.to_json
           }
           r.get("refunds", String, String) { |dt, route|
             Ns::Refund.where(s_date: dt.to_date).and(:settle.ne => 1).and(doc_type: "Ns::RefundOrder::#{route.classify}").limit(100).all.to_json
