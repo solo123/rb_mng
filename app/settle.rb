a25251f9eceae0a633aea0615f77dc6c00fb96b9
jimmy liang <576735357@qq.com>
2022-04-14 01:50:11 +0800

    pages

--- app/settle.rb
+++ app/settle.rb
@@ -2,30 +2,35 @@ module Ns
   module Route
     class App < Roda
       hash_branch("settle") do |r|
+        page_size = r.params['page_size']&.to_i || 50
+        cnd = {}
+        cnd.merge!({_id: {'$gt': r.params['big_id'].to_i}}) if r.params['big_id']
+        cnd.merge!({_id: {'$lt': r.params['small_id'].to_i}}) if r.params['small_id']
+
         r.get('index') {
-          Ns::Settlement.order(_id: -1).limit(20).all.to_a
+          Ns::Settlement.where(cnd).order(_id: -1).limit(page_size).all.to_a
         }
         r.get(String){|dt|
           Ns::Settlement.find_by(s_date: dt.to_date).to_a
         }
         r.on("errors") {
           r.get("statements", String) { |dt|
-            Ns::ChannelStatement.where(w_date: dt.to_date).and(:settle.ne => 1).limit(100).all.to_a
+            Ns::ChannelStatement.where(w_date: dt.to_date).and(:settle.ne => 1).and(cnd).order(_id: 1).limit(page_size).all.to_a
           }
           r.get("statements", String, String) { |dt, route|
-            Ns::ChannelStatement.where(w_date: dt.to_date).and(:settle.ne => 1).and(route: route.classify).limit(100).all.to_a
+            Ns::ChannelStatement.where(w_date: dt.to_date).and(:settle.ne => 1).and(route: route.classify).and(cnd).order(_id: 1).limit(page_size).all.to_a
           }
           r.get("orders", String) { |dt|
-            Ns::PayOrder.where(s_date: dt.to_date).and(:settle.ne => 1).limit(100).all.to_a
+            Ns::PayOrder.where(s_date: dt.to_date).and(:settle.ne => 1).and(cnd).order(_id: 1).limit(page_size).all.to_a
           }
           r.get("orders", String, String) { |dt, route|
-            Ns::PayOrder.where(s_date: dt.to_date).and(:settle.ne => 1).and(doc_type: "Ns::PayOrder::#{route.classify}").limit(100).all.to_a
+            Ns::PayOrder.where(s_date: dt.to_date).and(:settle.ne => 1).and(doc_type: "Ns::PayOrder::#{route.classify}").and(cnd).order(_id: 1).limit(page_size).all.to_a
           }
           r.get("refunds", String) { |dt|
-            Ns::Refund.where(s_date: dt.to_date).and(:settle.ne => 1).limit(100).all.to_a
+            Ns::Refund.where(s_date: dt.to_date).and(:settle.ne => 1).and(cnd).order(_id: 1).limit(page_size).all.to_a
           }
           r.get("refunds", String, String) { |dt, route|
-            Ns::Refund.where(s_date: dt.to_date).and(:settle.ne => 1).and(doc_type: "Ns::RefundOrder::#{route.classify}").limit(100).all.to_a
+            Ns::Refund.where(s_date: dt.to_date).and(:settle.ne => 1).and(doc_type: "Ns::RefundOrder::#{route.classify}").and(cnd).order(_id: 1).limit(page_size).all.to_a
           }
         }
       end
