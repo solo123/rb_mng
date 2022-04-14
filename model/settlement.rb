50c8ba6326025cb181a8bd75ed52ac7ddc930e43
jimmy liang <576735357@qq.com>
2022-04-14 13:08:54 +0800

    settlement fields

--- model/settlement.rb
+++ model/settlement.rb
@@ -8,7 +8,7 @@ module Ns
     index({ s_date: 1 }, { unique: true, name: "s_date_index" })
 
     def self.main_fields
-      only(:id, :s_date)
+      only(:id, :s_date, :statements_count, :orders_count, :refunds_count, :statements_error_count, :orders_error_count, :refunds_error_count)
     end
   end
 end
