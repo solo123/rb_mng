81903d6f2b67fff9b607295a2ff63983ddd1d0e6
jimmy liang <576735357@qq.com>
2022-04-14 11:50:45 +0800

    page

--- app/settle.rb
+++ app/settle.rb
@@ -5,14 +5,14 @@ module Ns
         page_size = r.params['page_size']&.to_i || 50
         cnd = {}
         r.get('index') {
-          cnd = {_id: {'$lt': r.params['last_id'.to_i]}} if r.params['last_id']
+          cnd = {_id: {'$lt': r.params['last_id'].to_i}} if r.params['last_id']
           Ns::Settlement.where(cnd).order(_id: -1).limit(page_size).all.map{|s| s.attributes.slice(:_id,:s_date,:orders_count,:refunds_count,:statements_count,:orders_error_count,:refunds_error_count,:statements_count)}
         }
         r.get(String){|dt|
           Ns::Settlement.find_by(s_date: dt.to_date).attributes
         }
         r.on("errors") {
-          cnd = {_id: {'$gt': r.params['last_id'.to_i]}} if r.params['last_id']
+          cnd = {_id: {'$gt': r.params['last_id'].to_i}} if r.params['last_id']
           r.get("statements", String) { |dt|
             Ns::ChannelStatement.where(w_date: dt.to_date).and(:settle.ne => 1).and(cnd).order(_id: 1).limit(page_size).all.to_a
           }
