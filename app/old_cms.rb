1949ff795ed319a234c1e5a97128e09445a52758
jimmy liang <576735357@qq.com>
2022-05-11 01:24:40 +0800

    export

--- app/old_cms.rb
+++ app/old_cms.rb
@@ -43,6 +43,29 @@ module Mng
             dts = get_date_array(r.params['type'], r.params['month'] || r.params['year'])
             old_format_output(pt, cnd, dts).merge(@debug)
           }
+          r.post('export_by_platform'){
+            fn = "plt#{Zlib.crc32(@t.to_json)}.csv"
+            full_fn = "#{NS::AppConfig.download_dir}/#{fn}"
+            unless File.exist?(full_fn)
+              pls = get_merchant_by_id(r.params['merchant_id']).sub_platform_ids
+              r.halt(200, {}) unless pls && !pls.empty?
+
+              pt = Hash.new{|h,k| h[k]=h.dup.clear}
+              q = Mng::QueryTrade.new
+              cnd = q.translate_query_condition(@t)
+              q.platform_static(cnd, pls).each do |d|
+                pt[d['_id']['pid']][d['_id']['s_date']] = {amount: d['amount'], cnt: d['cnt'], refund: d['refund'], active_cnt: d['active_cnt']}
+              end
+              dts = get_date_array(r.params['type'], r.params['month'] || r.params['year'])
+              data = old_format_output(pt, cnd, dts)
+
+              CSV.open(full_fn, "w", force_quotes: true) do |csv|
+                data_2_csv(csv, data[:data])
+                sm_2_csv(csv, data[:summary])
+              end
+            end
+            {code: 0, data: "https://ws.service.pooul.com/#{fn}"}
+          }
 
           r.post("search_by_partner"){
             m = get_merchant_by_id(r.params['merchant_id'])
@@ -113,7 +136,7 @@ module Mng
           }
           r.post('export_by_platform'){
             fn = "mch_plt#{Zlib.crc32(@t.to_json)}.csv"
-            full_fn = "/home/jimmy/tmp/" + fn
+            full_fn = "#{NS::AppConfig.download_dir}/#{fn}"
             unless File.exist?(full_fn)
               pls = get_merchant_by_id(r.params['merchant_id']).sub_platform_ids
               pt = Hash.new{|h,k| h[k]=h.dup.clear}
